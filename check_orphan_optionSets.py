f9674e07a091a9211fcf56807458570673d16e4b
Carlos Tejo <info@dayures.net>
2022-09-01 13:06:30 +0200

    As attributes can be linked to an OptionSet, added to the validation of the orphan optionSets

--- check_orphan_optionSets.py
+++ check_orphan_optionSets.py
@@ -18,19 +18,21 @@ if __name__ == "__main__":
     optionSet_resources = utils.get_resources_from_online(credentials=credentials, resource_type="optionSets", fields="id,name")
     tea_resources = utils.get_resources_from_online(credentials=credentials, resource_type="trackedEntityAttributes", fields="id,name,optionSet[id,name],optionSetValue", param_filter="filter=optionSetValue:eq:true")
     dataElement_resources = utils.get_resources_from_online(credentials=credentials, resource_type="dataElements", fields="id,name,optionSet[id,name],optionSetValue", param_filter="filter=optionSetValue:eq:true")
+    attributes_resources = utils.get_resources_from_online(credentials=credentials, resource_type="attributes", fields="id,name,optionSet[id,name]",param_filter="filter=optionSet:!null")
 
     # Filling the arrays with the IDs from OptionSets, dataElements and trackedEntityAttributes
     optionsets_uid = [os["id"] for os in optionSet_resources["optionSets"]]
     optionsets_dict = {os["id"] : os["name"] for os in optionSet_resources["optionSets"]}
 
-    teas_uid = [attribute["optionSet"]["id"] for attribute in tea_resources["trackedEntityAttributes"]]
-    des_uid = [element["optionSet"]["id"] for element in dataElement_resources["dataElements"]]
+    teas_uid = [tea["optionSet"]["id"] for tea in tea_resources["trackedEntityAttributes"]]
+    des_uid = [de["optionSet"]["id"] for de in dataElement_resources["dataElements"]]
+    atts_uid = [att["optionSet"]["id"] for att in attributes_resources["attributes"]]
            
-    optionset_not_used = [os for os in optionsets_uid if os not in teas_uid+des_uid]
+    optionset_not_used = [os for os in optionsets_uid if os not in teas_uid+des_uid+atts_uid]
     if optionset_not_used:
-        message = f"{len(optionset_not_used)} optionSets are not used by any TEA/DE: ({optionset_not_used})"
+        message = f"{len(optionset_not_used)} optionSets are not used by any TEA/DE/attribute: ({optionset_not_used})"
         logger.warn(message)
 
         for os in optionset_not_used:
-            message = f"The optionSet '{optionsets_dict[os]}' ({os}) is not used by any TEA/DE."
+            message = f"The optionSet '{optionsets_dict[os]}' ({os}) is not used by any TEA/DE/attribute."
             logger.warn(message)
