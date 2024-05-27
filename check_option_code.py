e12ce272e8a79201821f597fc33d44a74152555a
Carlos Tejo <1654811+dayures@users.noreply.github.com>
2024-05-27 10:26:51 +0200

    fix potential Catastrophic Backtracking

--- check_option_code.py
+++ check_option_code.py
@@ -6,8 +6,8 @@ import os
 import re
 
 
-PATTERN_CODE = re.compile("^([a-zA-Z0-9\_\|\-\.]+)+$") # upper and lower case letters, digits, '_', '|', '-', '.'
-#PATTERN_CODE = re.compile("^([0-9A-Z_\|\-\.]+)+$") # upper case, _ , -, |
+PATTERN_CODE = re.compile("^([a-zA-Z0-9\_\|\-\.])+$") # upper and lower case letters, digits, '_', '|', '-', '.'
+#PATTERN_CODE = re.compile("^([0-9A-Z_\|\-\.])+$") # upper case, _ , -, |
 
 def is_valid_code(code):
     return PATTERN_CODE.search(code)
